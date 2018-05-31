0434391a87304605e5771a754c81f25708424420
forafish <fora.luo@gmail.com>
2018-05-31 18:11:12 +0800

    add interaction doc-types

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import DocTypes from './constants/doc-types';
+import CollectionEntity from './entities/collection.entity';
+import FavoriteEntity from './entities/favorite.entity';
+
+export default {
+  DocTypes,
+  CollectionEntity,
+  FavoriteEntity
+};
